7e8b30f648e2a421f9c33414f12de5046b1198f4
christianwl <chris2w0social@gmail.com>
2025-07-09 05:31:39 -0300

    feat: iniciar estrutura do projeto

---
+++ src/br/com/alura/conversordemoedas/principal/Principal.java
@@ -0,0 +1,10 @@
+package br.com.alura.conversordemoedas.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+    }
+}
