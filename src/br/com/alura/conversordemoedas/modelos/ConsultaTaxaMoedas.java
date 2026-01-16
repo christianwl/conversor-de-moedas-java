015756d4a1b222665d4a964224e78adfa6a82f51
christianwl <chris2w0social@gmail.com>
2026-01-16 06:08:18 -0300

    refactor: alterar uso de variaveis de ambiente

--- src/br/com/alura/conversordemoedas/modelos/ConsultaTaxaMoedas.java
+++ src/br/com/alura/conversordemoedas/modelos/ConsultaTaxaMoedas.java
@@ -1,15 +1,17 @@
 package br.com.alura.conversordemoedas.modelos;
 
-import br.com.alura.conversordemoedas.config.Config;
 import br.com.alura.conversordemoedas.excecoes.InvalidKeyException;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class ConsultaTaxaMoedas extends ConexaoHttp {
-    private String apiKey = Config.getApiKey();
+    private String apiKey = System.getenv("EXCHANGE_API_KEY");
 
     public CambioMoeda buscarMoeda(String moeda){
+        if (apiKey == null){
+            throw new InvalidKeyException("A variável de ambiente EXCHANGE_API_KEY não foi configurada!");
+        }
         String endereco = "https://v6.exchangerate-api.com/v6/" + this.apiKey +"/latest/" + moeda;
         Gson gson = new GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
